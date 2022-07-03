32b225b9dc38af7d061d4a5ea98c20d1ed8848d4
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-03 19:19:12 -0400

    added README template in generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,39 @@
-const generateReadMe = () => {
+// Function to generate markdown for README
+
+const generateReadMe = answers => {
   return `
+  # ${answers.title}
+  
+  ![Github licence](http://img.shields.io/badge/license-${answers.license}-blue.svg)
+
+  ## Description
+  ${answers.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  This project is license under ${answers.license}
+
+  ## Contributing
+  ${answers.contributors}
+  
+  ## Tests
+  ${answers.test}
+
+  ## Questions
+  ${answers.usage}
   
   `
 }
